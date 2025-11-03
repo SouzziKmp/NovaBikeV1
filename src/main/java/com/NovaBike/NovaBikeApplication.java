b81c23b47cc9787ce87643da878aca525035c1e1
SouzziKmp <scampos30549@ufide.ac.cr>
2025-11-03 13:34:21 -0600

    Commit 0  Inicialización

---
+++ src/main/java/com/NovaBike/NovaBikeApplication.java
@@ -0,0 +1,13 @@
+package com.NovaBike;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NovaBikeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NovaBikeApplication.class, args);
+	}
+
+}
